6219d5b0df9f4545b59c4bcf6e03133e0d18e0c0
sisu <mikko.sysikaski@gmail.com>
2009-02-22 13:33:02 +0200

    Added creating test docs to pdfgen.sh.

--- pdfgen.sh
+++ pdfgen.sh
@@ -2,11 +2,19 @@
 
 mkdir -p tex
 cd tex
-CLASSPATH="$CLASSPATH:tmp" javadoc -private -author -version -sourcepath ../src/ -docletpath texdoclet.jar -doclet org.wonderly.doclets.TexDoclet -title "Luolalentely verkossa" -author "Mikko Sysikaski" lento.gamestate lento.gameui lento.net lento.menu
+CLASSPATH="$CLASSPATH:tmp" javadoc -private -author -version -sourcepath ../src/ -docletpath texdoclet.jar -doclet org.wonderly.doclets.TexDoclet -title "Liite D: Ohjelman API-kuvaus" -author "Mikko Sysikaski" lento.gamestate lento.gameui lento.net lento.menu
 head -1 docs.tex > lento.tex
 echo "\usepackage[utf8]{inputenc}" >> lento.tex
 tail +2 docs.tex >> lento.tex
 latex lento.tex
 dvipdf lento.dvi
-okular lento.pdf &
+#okular lento.pdf &
+
+CLASSPATH="$CLASSPATH:tmp:../classes/:/usr/lib/junit.jar" javadoc -private -author -version -sourcepath ../test/ -docletpath texdoclet.jar -doclet org.wonderly.doclets.TexDoclet -title "Liite E: Testien API-kuvaus" -author "Mikko Sysikaski" lento.gamestate lento.gameui lento.net
+head -1 docs.tex > test.tex
+echo "\usepackage[utf8]{inputenc}" >> test.tex
+tail +2 docs.tex >> test.tex
+latex test.tex
+dvipdf test.dvi
+
 cd -
